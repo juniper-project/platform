5a4e60389834dee90b1bfd29f755993acf97397d
Alexey Cheptsov <cheptsov@hlrs.de>
2015-11-20 14:30:19 +0100

    Fixed running on multi-core nodes

--- JUNIPER.Application.MapReduce/run_2maps.sh
+++ JUNIPER.Application.MapReduce/run_2maps.sh
@@ -1,5 +1,5 @@
 if [ -f "hosts" ]; then
-    mpirun -machinefile hosts --oversubscribe -np 6 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_2maps.xml
+    mpirun -machinefile hosts --oversubscribe --map-by node -np 6 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_2maps.xml
 else
     mpirun -np 6 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_2maps.xml
 fi
--- JUNIPER.Application.MapReduce/run_3maps.sh
+++ JUNIPER.Application.MapReduce/run_3maps.sh
@@ -1,5 +1,5 @@
 if [ -f "hosts" ]; then
-    mpirun -machinefile hosts --oversubscribe -np 8 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_3maps.xml
+    mpirun -machinefile hosts --oversubscribe --map-by node -np 8 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_3maps.xml
 else
     mpirun -np 8 java -jar `pwd`/target/mapreduce-1.0-jar-with-dependencies.jar `pwd`/application_model_3maps.xml
 fi
